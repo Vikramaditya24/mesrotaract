3ec0f283ce0b84f218850f4d65c6cad3431bda02
Vikramaditya24 <81467681+Vikramaditya24@users.noreply.github.com>
2022-03-11 17:33:54 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MESRaC_3190',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
+
 
 
 # Password validation
